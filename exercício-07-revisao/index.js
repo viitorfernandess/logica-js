b7f01fc7fc5b75d6be6b43da4600294073c7c50d
viitorfernandess <vitorazevedofer007@gmail.com>
2026-02-06 18:44:36 -0300

    feat: adiciona lógica para calcular média dos valores do array

--- exercício-07-revisao/index.js
+++ exercício-07-revisao/index.js
@@ -1,11 +1,35 @@
 const numeros = [5, 12, 8, 3, 21, 7]
 
-let maior = numeros[0]
+// DESCOBRIR O MAIOR NÚMERO
+// let maior = numeros[0]
 
-for(i = 0; i < numeros.length; i++) {
-    if( numeros[i] > maior ) {
-        maior = numeros[i]
-    }
+// for(i = 0; i < numeros.length; i++) {
+//     if( numeros[i] > maior ) {
+//         maior = numeros[i]
+//     }
+// }
+
+// console.log(maior)
+
+// DESCOBRIR O MENOR NÚMERO
+
+// let menor = numeros[0]
+
+// for(i = 1; i < numeros.length; i++) {
+//     if(numeros[i] < menor) {
+//         menor = numeros[i]
+//     }
+// }
+
+// console.log(menor)
+
+// DESCOBRIR A MÉDIA DOS VALORES
+
+let soma = 0
+let media = 0
+
+for (i = 0; i < numeros.length; i++) {
+    soma = soma + numeros[i]
 }
-
-console.log(maior)
+media = soma / numeros.length
+console.log(media.toFixed(2))
