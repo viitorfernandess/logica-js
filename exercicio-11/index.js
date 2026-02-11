6c11be2eac14b4f2d2441921132954a8c48304a3
viitorfernandess <vitorazevedofer007@gmail.com>
2026-02-11 10:46:36 -0300

    add: estrutura inicial de dados ( array de objetos pessoas)

---
+++ exercicio-11/index.js
@@ -0,0 +1,7 @@
+const pessoas = [
+  { nome: "Ana", idade: 17, renda: 1500, pcd: false },
+  { nome: "Carlos", idade: 65, renda: 4000, pcd: false },
+  { nome: "Marina", idade: 30, renda: 2000, pcd: true },
+  { nome: "João", idade: 10, renda: 3500, pcd: false },
+  { nome: "Fernanda", idade: 70, renda: 2500, pcd: false }
+]
