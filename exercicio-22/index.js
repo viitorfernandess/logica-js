526886f1f2f6f70fb07b21678b57fc59a9d284dd
viitorfernandess <vitorazevedofer007@gmail.com>
2026-03-26 10:57:08 -0300

    feat: implementa funçao comparação recursiva de árvores binárias

--- exercicio-22/index.js
+++ exercicio-22/index.js
@@ -15,10 +15,21 @@ p.right = new TreeNode(3)
 // raíz árvore q
 const q = new TreeNode(1)
 q.left = new TreeNode(2)
-q.right  = new TreeNode(2)
+q.right = new TreeNode(2)
 
 console.log(p, q)
 
 function comparacao(p, q) {
-    
+    if (p === null && q === null) {
+        return true
+    }
+    if (p === null || q === null) {
+        return false
+    }
+    if (p.val !== q.val) {
+        return false
+    }
+    return comparacao(p.left, q.left) && comparacao(p.right, q.right)
 }
+
+console.log(comparacao(p,q))
