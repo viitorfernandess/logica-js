edf9ac3748a0019003d1e5ebfe649b8d43597143
viitorfernandess <vitorazevedofer007@gmail.com>
2026-04-28 10:54:33 -0300

    feat: implementar função printList para lista encadeada

---
+++ exercicio-38/index.js
@@ -0,0 +1,28 @@
+class ListNode {
+    constructor(val, next = null) {
+        this.val = val
+        this.next = next
+    }
+}
+
+// Montando a lista [1,2,3,4,5]
+const head = new ListNode(1)
+head.next = new ListNode(2)
+head.next.next = new ListNode(3)
+head.next.next.next = new ListNode(4)
+head.next.next.next.next = new ListNode(5)
+
+function printList(head) {
+    let list = ""
+    let current = head
+
+    while (current != null) {
+        list += current.val + " -> "
+        current = current.next
+    }
+    list += "null"
+    return list
+}
+
+
+console.log(printList(head))
