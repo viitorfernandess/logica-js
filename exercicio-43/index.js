0cbcbb27285068f0b38bcf043a9c9fa3f950d179
viitorfernandess <vitorazevedofer007@gmail.com>
2026-05-04 15:56:08 -0300

    refactor: otimiza Two Sum para O(n) usando hash map

--- exercicio-43/index.js
+++ exercicio-43/index.js
@@ -1,13 +1,27 @@
 const nums = [2, 7, 11, 15]
 const target = 9
 
+// function twoSum(nums, target) {
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] + nums[j] === target) {
+//                 return [i, j]
+//             }
+//         }
+//     }
+// }
+
 function twoSum(nums, target) {
+    const map = {}
+
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j]
-            }
+        const complemento = target - nums[i]
+
+        if (map[complemento] !== undefined) {
+            return [map[complemento], i]
         }
+
+        map[nums[i]] = i
     }
 }
 
