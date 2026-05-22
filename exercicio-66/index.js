d2a5c236338a5d0ae70bd8d255baf0b65620f1cf
viitorfernandess <vitorazevedofer007@gmail.com>
2026-05-22 14:39:24 -0300

    refactor: revisa algoritmo de interseção entre arrays

--- exercicio-66/index.js
+++ exercicio-66/index.js
@@ -1,13 +1,16 @@
+// Dado dois arrays,
+// retorne os elementos que existem em ambos,
+// sem repetir valores no resultado.
+
 const arr1 = [1, 2, 1, 2]
-const arr2 = [2, 2]
+const arr2 = [2, 3]
 
 function intersection(arr1, arr2) {
     let newArr = []
 
     for (let i = 0; i < arr1.length; i++) {
         for (let j = 0; j < arr2.length; j++) {
-            if (arr2[j] === arr1[i]) {
-
+            if (arr1[i] === arr2[j]) {
                 if (!newArr.includes(arr2[j])) {
                     newArr.push(arr2[j])
                 }
