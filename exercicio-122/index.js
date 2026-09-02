a9aa1cff366ce32c5e7fe00b54fc32935b6316bc
viitorfernandess <vitorazevedofer007@gmail.com>
2026-09-02 15:53:40 -0300

    feat: resolve exdercício de strings isomórficas

---
+++ exercicio-122/index.js
@@ -0,0 +1,37 @@
+// O exercício pede para verificar se duas strings são isomórficas.
+// Cada caractere de s deve sempre ser mapeado para o mesmo caractere de t, 
+// e caracteres diferentes de s não podem ser mapeados para o mesmo caractere de t.
+
+function isomorphicStrings(s, t) {
+
+    if (s.length !== t.length) {
+        return false
+    }
+
+    const mapa = {}
+    const mapaInverso = {}
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] in mapa) {
+
+            if (mapa[s[i]] !== t[i]) {
+                return false
+            }
+        } else {
+            mapa[s[i]] = t[i]
+        }
+
+        if (t[i] in mapaInverso) {
+
+            if (mapaInverso[t[i]] !== s[i]) {
+                return false
+            }
+        } else {
+            mapaInverso[t[i]] = s[i]
+        }
+    }
+
+    return true
+}
+
+console.log(isomorphicStrings("aa", "cb"))
